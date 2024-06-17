15474526d346879208b5747f04b2cbfc03340598
Rushabh <141929113+RushabhShah-1@users.noreply.github.com>
2024-06-17 13:55:55 +0530

    Create Logout.jsx

---
+++ src/components/account/Logout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+const Logout = ({ setIsAuthenticated }) => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    sessionStorage.clear();
+    setIsAuthenticated(false);
+    setTimeout(() => navigate("/"), 3000);
+  }, [navigate, setIsAuthenticated]);
+  return (
+    <>
+      <h1>You are successfully logged out.</h1>
+      <h2>You will be redirected to login in 3 seconds.</h2>
+    </>
+  );
+};
+
+export default Logout;
