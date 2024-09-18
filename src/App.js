c3d2b1a70151e0673a5f3bd21de9e29a90ce14da
Rushabh <141929113+RushabhShah-1@users.noreply.github.com>
2024-09-18 10:34:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
               </Route>
                <Route
                 path="/"
-                element={<PrivateRoute isAuthenticated={isAuthenticated} />}
+                element={<PrivateRoute isAuthenticated={true} />}
               >
                 <Route path="/" element={<Home />} />
               </Route>   
