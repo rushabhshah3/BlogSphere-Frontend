f04f548880a62242ba1fe223b5b426f81a4f942d
Rushabh <141929113+rushabhshah3@users.noreply.github.com>
2024-09-24 11:17:11 +0530

    Update DetailView.jsx

--- src/components/details/DetailView.jsx
+++ src/components/details/DetailView.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState, useEffect, useContext } from "react";
 
 import { Box, Typography, styled } from "@mui/material";
-// import { Delete, Edit } from "@mui/icons-material";
+import { Delete, Edit } from "@mui/icons-material";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 import { API } from "../../service/api";
@@ -26,19 +26,19 @@ const Image = styled("img")({
   maxHeight: "35rem",
 });
 
-// const EditIcon = styled(Edit)`
-//   margin: 5px;
-//   padding: 5px;
-//   border: 1px solid #878787;
-//   border-radius: 10px;
-// `;
+const EditIcon = styled(Edit)`
+  margin: 5px;
+  padding: 5px;
+  border: 1px solid #878787;
+  border-radius: 10px;
+`;
 
-// const DeleteIcon = styled(Delete)`
-//   margin: 5px;
-//   padding: 5px;
-//   border: 1px solid #878787;
-//   border-radius: 10px;
-// `;
+const DeleteIcon = styled(Delete)`
+  margin: 5px;
+  padding: 5px;
+  border: 1px solid #878787;
+  border-radius: 10px;
+`;
 
 const Heading = styled(Typography)`
   font-size: 38px;
@@ -61,7 +61,7 @@ const DetailView = () => {
     "https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80";
 
   const [post, setPost] = useState({});
-  // const { account } = useContext(DataContext);
+  const { account } = useContext(DataContext);
 
   const navigate = useNavigate();
   const { id } = useParams();
@@ -76,15 +76,15 @@ const DetailView = () => {
     fetchData();
   }, [id]);
 
-  // const deleteBlog = async () => {
-  //   await API.deletePost(post._id);
-  //   navigate("/");
-  // };
+  const deleteBlog = async () => {
+    await API.deletePost(post._id);
+    navigate("/");
+  };
 
   return (
     <Container>
       <Image src={post.picture || url} alt="post" />
-{/*       <Box style={{ float: "right" }}>
+      <Box style={{ float: "right" }}>
         {account.username === post.username && (
           <>
             <Link to={`/update/${post._id}`}>
@@ -93,7 +93,7 @@ const DetailView = () => {
             <DeleteIcon onClick={() => deleteBlog()} color="error" />
           </>
         )}
-      </Box> */}
+      </Box>
       <Heading>{post.title}</Heading>
 
       <Author>
